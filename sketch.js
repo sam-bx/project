e53c0b9ef74ab89f284ccb30354f487ee9a69d80
sam-bx <107395247+sam-bx@users.noreply.github.com>
2022-07-27 13:33:58 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+var touches
+
+var plasticGroup, plastic1, plastic2 ;
+var poisonGroup, poison1, poison2 ;
+var foodGroup, food1, food2, food3, food4;
+var backgroundImg
+var score=0;
+var chompsound, deathsound;
+
+var gameOver, restart;
+
+function preload(){
+
+
+    
+}
+
+function setup(){
+
+
+
+createCanvas(600,600)
+
+
+}
+
+
+
+
+function draw() {
+  
+    drawSprites()
+}
+   
+function spawnplastic(){
+
+
+
+
+}
+
+
+function spawnpoison(){
+
+
+
+
+
+
+
+}
+
+
+function spawnfood(){
+
+
+
+
+    
+}
+    
